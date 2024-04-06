f2c4cb74d2371db75491966b8e0ce62ab3de52f9
Harshalgbj <125290125+harshalgangurde44@users.noreply.github.com>
2024-04-06 11:35:39 +0530

    img add

--- src/components/FoodItemSelector.js
+++ src/components/FoodItemSelector.js
@@ -2,11 +2,17 @@ import { CDN_URL } from "./contants";
 import { Link } from "react-router-dom";
 import React from "react";
 
-const FoodItemSelector = ({ id, name, price, imageId, description }) => {
+const FoodItemSelector = ({
+  id,
+  name,
+  price,
+  cloudinaryImageId,
+  description,
+}) => {
   return (
     <div className=" w-56 p-2 m-2 shadow-lg bg-orange-50">
       <div className="res-card">
-        <img className="img-card" src={CDN_URL + imageId} />
+        <img className="img-card" src={CDN_URL + cloudinaryImageId} />
         <h2 className=" font-bold text-lg">{name}</h2>
         <h4>{description}</h4>
         <h4 className=" font-bold">Price-{price / 100}</h4>
